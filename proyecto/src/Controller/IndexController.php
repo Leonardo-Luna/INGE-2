b69ebd11e4201f768ed827a4e7d0055ce46f8e7c
Leonardo Luna <lunaleonardo031@gmail.com>
2025-05-26 22:55:22 -0300

    Feat: Integración con Nominatim OpenStreetMap y Leaflet

--- proyecto/src/Controller/IndexController.php
+++ proyecto/src/Controller/IndexController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
+use App\Services\MapService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
-    public function __construct(private RequestStack $requestStack) { }
+    public function __construct(private MapService $mapService) { }
 
     #[Route('/', name: 'app_index')]
     public function index(Request $request): Response
     {
         // Acá iría las cosas de sucursales, que es nuestro index
         return $this->render('index/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/test/mapa', name: 'app_mapa')]
+    public function testMapa(Request $request): Response
+    {
+        $direccion = "Calle 64 525";
+
+        $coordenadas = $this->mapService->calcularCoordenadas($direccion);
+
+        return $this->render('index/mapa.html.twig', [
+            'lat' => $coordenadas['lat'] ?? '',
+            'lon' => $coordenadas['lon'] ?? '',
         ]);
     }
 }
